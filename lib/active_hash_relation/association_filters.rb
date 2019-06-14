e7abcce220ae0772d10f49fd622d834349bd44db
Adam Marťák <adam.martak@railsformers.com>
2019-06-14 10:23:42 +0200

    fix

--- lib/active_hash_relation/association_filters.rb
+++ lib/active_hash_relation/association_filters.rb
@@ -18,7 +18,7 @@ module ActiveHashRelation::AssociationFilters
             ).apply_filters
           else
             association_filters = self.filter_class(association_name).new(
-              association_name.singularize.constantize.all,
+              model.reflect_on_association(association).class_name.constantize.unscoped.all,
               params[association]
             ).apply_filters
           end
@@ -31,7 +31,7 @@ module ActiveHashRelation::AssociationFilters
             ).apply_filters
           else
             association_filters = ActiveHashRelation::FilterApplier.new(
-              association.class_name.constantize.unscoped.all,
+              model.reflect_on_association(association).class_name.constantize.unscoped.all,
               params[association],
               include_associations: true
             ).apply_filters
